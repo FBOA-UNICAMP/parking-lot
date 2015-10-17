056f078423254e4b0a2688c7ef37451f7460e1fc
Marcelo Mingatos <marcelo.m.toledo@gmail.com>
2015-10-17 11:26:09 -0300

    Changed Sensor Start Message

--- org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/Activator.java
+++ org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/Activator.java
@@ -10,7 +10,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Sensor Start Message");
+		System.out.println("Message1");
 	}
 	
 	/*
