486a054af5d1819d7b43e0a73438ea7b33e434fb
Matheus Pomaleski <matheuspomaleski@gmail.com>
2015-10-17 19:06:38 -0300

    Fixing a bug on the random timer and adding timestamp to the printed messages

--- org.eclipse.concierge.example.parking_lot.client/src/org/eclipse/concierge/example/parking_lot/service/impl/PanelServiceImpl.java
+++ org.eclipse.concierge.example.parking_lot.client/src/org/eclipse/concierge/example/parking_lot/service/impl/PanelServiceImpl.java
@@ -1,11 +1,20 @@
 package org.eclipse.concierge.example.parking_lot.service.impl;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.eclipse.concierge.example.parking_lot.service.PanelService;
 
 public class PanelServiceImpl implements PanelService {
+	DateFormat dateFormat;
+	public PanelServiceImpl(){
+		dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	}
 	
 	@Override
 	public void updatePanelWithSensorAndStatus(int sensorId, ParkingSpaceStatus status) {
-		System.out.println("Sensor: " + sensorId + "; status: " + status.toString());	
+		Calendar cal = Calendar.getInstance();
+		System.out.println(dateFormat.format(cal.getTime()) + ": Sensor[" + sensorId + "]'s status: " + status.toString());	
 	}
 }
--- org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/Sensor.java
+++ org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/Sensor.java
@@ -7,21 +7,53 @@ import java.util.Random;
 import javax.swing.Timer;
 import org.eclipse.concierge.example.parking_lot.service.PanelService.ParkingSpaceStatus;
 
-public class Sensor implements ActionListener {
+public class Sensor {
 	
 	private int id;
 	private ParkingSpaceStatus status;
 	private SensorDelegate delegate;
-	private int minTime = 1000;
+	private int minTime = 500;
 	private int maxTime = 30000;
+	private int fixedTime = 10000;
 	Random rand;
 	Timer T1;
+	Timer T2;
 	
-	public Sensor() {
+	private class ListenerT1 implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(status == ParkingSpaceStatus.FREE)
+				status = ParkingSpaceStatus.BUSY;
+			else status = ParkingSpaceStatus.FREE;
+			delegate.sensorStatusDidChange(id, status);
+			T1.stop();
+			int randTime = minTime + rand.nextInt(maxTime-minTime+1);
+			T1.setInitialDelay(randTime);
+			T2.stop();
+			T1.restart();
+			T2.restart();
+		}
+	}
+	
+	private class ListenerT2 implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			delegate.sensorStatusDidChange(id, status);
+		}
+		
+	}
+	
+	public Sensor(){
 		rand = new Random();
-		T1 = new Timer(minTime + rand.nextInt(maxTime-minTime+1), this);
+		int randTime = minTime + rand.nextInt(maxTime-minTime+1);
+		System.out.println(randTime);
+		T1 = new Timer(randTime, new ListenerT1());
+		T2 = new Timer(fixedTime, new ListenerT2());
 		status = ParkingSpaceStatus.FREE;
 		T1.start();
+		T2.start();
 	}
 	
 	public Sensor(int id, ParkingSpaceStatus status, SensorDelegate delegate){
@@ -29,8 +61,11 @@ public class Sensor implements ActionListener {
 		this.status = status;
 		this.delegate = delegate;
 		rand = new Random();
-		T1 = new Timer(minTime + rand.nextInt(maxTime-minTime+1), this);
+		int randTime = minTime + rand.nextInt(maxTime-minTime+1);
+		T1 = new Timer(randTime, new ListenerT1());
+		T2 = new Timer(fixedTime, new ListenerT2());
 		T1.start();
+		T2.start();
 	}
 	
 	public ParkingSpaceStatus getStatus() {
@@ -54,16 +89,6 @@ public class Sensor implements ActionListener {
 		this.delegate = delegate;
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		if(this.status == ParkingSpaceStatus.FREE)
-			this.status = ParkingSpaceStatus.BUSY;
-		else this.status = ParkingSpaceStatus.FREE;
-		this.delegate.sensorStatusDidChange(this.id, status);
-		T1.stop();
-		T1.setDelay(minTime + rand.nextInt(maxTime-minTime+1));
-		T1.restart();
-	}
 }
 
 interface SensorDelegate {
