9d4891772b3e0c2638622dfa5b4643b3ba1003c9
Matheus Pomaleski <matheuspomaleski@gmail.com>
2015-10-17 17:14:00 -0300

    Added a status change on a random timer to simulate the sensor

--- org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/Sensor.java
+++ org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/Sensor.java
@@ -1,10 +1,37 @@
 package org.eclipse.concierge.example.parking_lot.sensor;
 
-public class Sensor {
-	
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.Timer;
+
+public class Sensor implements ActionListener {
 	private int id;
 	private ParkingSpaceStatus status;
 	private SensorDelegate delegate;
+	
+	private int minTime = 1000;
+	private int maxTime = 30000;
+	Random rand;
+	Timer T1;
+	
+	public Sensor(){
+		rand = new Random();
+		T1 = new Timer(minTime + rand.nextInt(maxTime-minTime+1), this);
+		status = ParkingSpaceStatus.FREE;
+		T1.start();
+	}
+	
+	public Sensor(int id, ParkingSpaceStatus status, SensorDelegate delegate){
+		this.id = id;
+		this.status = status;
+		this.delegate = delegate;
+		rand = new Random();
+		T1 = new Timer(minTime + rand.nextInt(maxTime-minTime+1), this);
+		status = ParkingSpaceStatus.FREE;
+		T1.start();
+	}
 	
 	public ParkingSpaceStatus getStatus() {
 		return status;
@@ -25,6 +52,17 @@ public class Sensor {
 	}
 	public void setDelegate(SensorDelegate delegate) {
 		this.delegate = delegate;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(this.status == ParkingSpaceStatus.FREE)
+			this.status = ParkingSpaceStatus.BUSY;
+		else this.status = ParkingSpaceStatus.FREE;
+		this.delegate.sensorStatusDidChange(this.id, status);
+		T1.stop();
+		T1.setDelay(minTime + rand.nextInt(maxTime-minTime+1));
+		T1.restart();
 	}
 }
 
