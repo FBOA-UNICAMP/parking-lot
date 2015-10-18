236fdf42045b6faa9a61dab66aaf2701939e33b2
Marcelo Mingatos <marcelo.m.toledo@gmail.com>
2015-10-18 11:24:23 -0200

    Sensor panel decoupling refactor and maintenance interface

--- org.eclipse.concierge.example.parking_lot.client/src/org/eclipse/concierge/example/parking_lot/service/PanelService.java
+++ org.eclipse.concierge.example.parking_lot.client/src/org/eclipse/concierge/example/parking_lot/service/PanelService.java
@@ -2,7 +2,9 @@ package org.eclipse.concierge.example.parking_lot.service;
 
 public interface PanelService {
 	public enum ParkingSpaceStatus {
-	    FREE, BUSY;
+	    AVAILABLE, 
+	    OCCUPIED,
+	    UNKNOWN;
 	}
 	public void updatePanelWithSensorAndStatus(int sensorId, ParkingSpaceStatus status);
 }
--- org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/impl/SensorMonitoringInterface.java
+++ org.eclipse.concierge.example.parking_lot.sensor/src/org/eclipse/concierge/example/parking_lot/sensor/impl/SensorMonitoringInterface.java
@@ -1,11 +1,11 @@
 package org.eclipse.concierge.example.parking_lot.sensor.impl;
 
-import org.eclipse.concierge.example.parking_lot.service.PanelService.ParkingSpaceStatus;
+import org.eclipse.concierge.example.parking_lot.sensor.impl.Sensor.SensorStatus;
 
 public interface SensorMonitoringInterface {
 	
-	public void sensorStatusDidChange(int id, ParkingSpaceStatus status);
+	public void sensorStatusDidChange(int id, SensorStatus status);
 	
-	public void sensorStatusTimeToUpdate(int id, ParkingSpaceStatus status);
+	public void sensorStatusTimeToUpdate(int id, SensorStatus status);
 	
 }
